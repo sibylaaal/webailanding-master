bb256c845f38b1d47c8cd89de116bf5aebdd5a7e
sibylaaal <bybill757@gmail.com>
2024-09-01 14:56:48 +0200

    HHHH

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,9 @@ class MyDocument extends Document {
       return (
          <Html lang="en">
             <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="icon" type="image/svg+xml" href="/logo.png" />
+            <title>webai</title>
+
             <link
                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
                rel="stylesheet"
